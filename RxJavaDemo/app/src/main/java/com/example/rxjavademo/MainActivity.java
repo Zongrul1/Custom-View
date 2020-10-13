80b8cac73b109aadaa14186153bbee4ae7d3ffd7
zongru <zongruli1996@gmail.com>
2020-10-13 23:51:20 +1100

    rxjava

---
+++ RxJavaDemo/app/src/main/java/com/example/rxjavademo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.rxjavademo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "rxjava";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("连载1");
+                emitter.onNext("连载2");
+                emitter.onNext("连载3");
+                emitter.onComplete();
+            }
+        })        .doOnNext(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Thread.sleep(10000);
+                Log.e(TAG,"doOnNext");
+            }
+        })
+                .observeOn(AndroidSchedulers.mainThread())//回调在主线程
+                .subscribeOn(Schedulers.io())//执行在io线程
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.e(TAG,"onSubscribe");
+                    }
+                    @Override
+                    public void onNext(String value) {
+                        Log.e(TAG,"onNext:"+value);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG,"onError="+e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG,"onComplete()");
+                    }
+                });
+    }
+}
